821220110e616fed81c4431282bae2f1fcc007ca
Adriel Bento <adrielbb15@gmail.com>
2023-04-03 09:10:29 -0300

    feat: ajusta para deploy no fly.io

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ server.post("/users", async (request, reply) => {
 
 server.listen({
     host: "0.0.0.0", 
-    port: process.env.PORT ? Number(process.env.PORT) : 3333
+    port: process.env.PORT ? Number(process.env.PORT) : 8080
 }).then(() => {
     console.log(`Server listening`);
 })
